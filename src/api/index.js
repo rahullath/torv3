e415eca89d9877c764d4165895b910bc1ea6e8f5
Rahul Lath <rahul.lath@science.christuniversity.in>
2023-09-14 12:44:54 +0530

    ok

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: `http://193.108.113.216:5000/api/`,
+    baseURL: `http://192.168.113.216:5000/api/`,
     withCredentials: true
 });
